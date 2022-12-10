311fb56cec7eb20bb19f354c77c20538508a32d2
ellenjane001 <ellenjane.000001998@gmail.com>
2022-12-11 00:50:31 +0800

    finalization

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ let App = {
             method: 'GET',
             mode: 'cors',
         })
-
     },
-    CONTAINER: document.getElementById('root')
+    CONTAINER: document.getElementById('root'),
+    // SEND data to API
+    POST: async (url = '', data = {}) => {
+        return await fetch(url, {
+            method: 'POST',
+            // mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    },
+    PUT: async (url = '', data = {}) => {
+        return await fetch(url, {
+            method: 'PUT',
+            // mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    },
 }
 let ctgry = ['case', 'cooling', 'display', 'gpu', 'input', 'mb', 'processor', 'ps', 'ram', 'sd']
 let DOM = {
@@ -46,8 +67,9 @@ let DOM = {
     dataManipulation: async (e) => {
         let p = []
         let res = await DOM.fetchList(e.target.getAttribute('data-value'))
-
         for (let i = 0; i < Object.keys(res).length; i++) {
+            res[Object.keys(res)[i]]['category'] = e.target.getAttribute('data-value')
+            res[Object.keys(res)[i]]['key'] = Object.keys(res)[i]
             p.push(res[Object.keys(res)[i]])
         }
         document.querySelectorAll(`li.nav-item>a`).forEach(a => {
@@ -57,10 +79,10 @@ let DOM = {
         DOM.clearElements(document.querySelector('.container.selector'))
         document.querySelector('.container.selector').appendChild(DOM.showDropdown(p))
         document.querySelector('select[class="m-2"]').dispatchEvent(new Event("change"))
+        document.querySelector('.btn.btn-outline-primary.mx-auto.text-center').dispatchEvent(new Event("click"))
     },
     showForm: (e) => {
         let data = JSON.parse(e.target.value)
-        console.log(data)
         let form = document.createElement('form')
         let formClasses = ['d-flex', 'flex-column', 'gap-3', 'w-100', 'mx-auto', 'py-3', 'align-items-start']
         for (let c = 0; c < formClasses.length; c++) {
@@ -80,6 +102,7 @@ let DOM = {
         name.type = 'text'
         name.classList.add('w-100')
         name.id = 'name'
+        name.name = 'name'
         name.value = data.name
         nameDiv.appendChild(nameLbl)
         nameDiv.appendChild(name)
@@ -125,20 +148,25 @@ let DOM = {
         submitBtn.classList.add('btn-outline-primary')
         submitBtn.classList.add('mx-auto')
         submitBtn.classList.add('text-center')
+        submitBtn.setAttribute('data-value', JSON.stringify(data))
         submitBtn.onclick = DOM.generateObj
         form.appendChild(submitBtn)
         DOM.clearElements(App.CONTAINER)
         App.CONTAINER.appendChild(form)
+        document.querySelector('.btn.btn-outline-primary.mx-auto.text-center').dispatchEvent(new Event("click"))
     },
     initialLoad: async () => {
         let p = []
         let res = await DOM.fetchList(ctgry[0])
         document.querySelectorAll(`li.nav-item>a[data-value="${ctgry[0]}"]`)[0].classList.add('active')
         for (let i = 0; i < Object.keys(res).length; i++) {
+            res[Object.keys(res)[i]]['category'] = ctgry[0]
+            res[Object.keys(res)[i]]['key'] = Object.keys(res)[i]
             p.push(res[Object.keys(res)[i]])
         }
         document.querySelector('.container.selector').appendChild(DOM.showDropdown(p))
         document.querySelector('select[class="m-2"]').dispatchEvent(new Event("change"))
+        document.querySelector('.btn.btn-outline-primary.mx-auto.text-center').dispatchEvent(new Event("click"))
     },
     clearElements: (element) => {
         element.innerHTML = ''
@@ -152,28 +180,67 @@ let DOM = {
         inputs.classList.add('w-100')
         inputs.classList.add('m-2')
         li.appendChild(inputs)
-        console.log(document.getElementById('imgLinks').appendChild(li))
     },
     generateObj: (e) => {
         e.preventDefault()
-        let obj = {}
-        console.log(document.querySelector(`.${Array.from(e.target.parentElement.classList).join('.')}`).getElementsByTagName('input'))
-        obj = {
-            ...obj
+        let data = JSON.parse(e.target.getAttribute('data-value'))
+        let obj = {
+            image_link: data.image_link || [],
+            name: data.name,
+            source: data.source,
+            specification: data.specification,
+            key: data.key,
+            category: data.category,
+            specs: data.specs || {}
         }
+        let form = document.querySelector(`.${Array.from(e.target.parentElement.classList).join('.')}`)
+        if (data.image_link.length < document.getElementById('imgLinks').children.length) {
+            obj['image_link'].push(form.querySelectorAll('.image_link')[document.getElementById('imgLinks').children.length - 1].value)
+        }
+        if (document.getElementById('name').value !== data.name) {
+            obj.name = document.getElementById('name').value
+        }
+        document.getElementsByTagName('pre')[0].innerHTML = JSON.stringify(obj, null, 3)
     },
     addProdSpec: () => {
-
+        let data = JSON.parse(document.getElementsByTagName('pre')[0].innerHTML)
+        let inputs = document.getElementById('add-product-spec').getElementsByTagName('input')[0]
+        let textArea = document.getElementById('add-product-spec').getElementsByTagName('textarea')[0]
+        data['specs'][`${inputs.value.replace(/\//g, '')}`] = textArea.value.replace(/\n/g, ',')
+        document.getElementsByTagName('pre')[0].innerHTML = JSON.stringify(data, null, 3)
+    },
+    updateData: (obj, key, data) => {
+        return response = App.PUT(`${App.URL}${obj}/${key}.json`, data).then(res => res.json())
     }
-
 }
 for (i in ctgry) {
     document.querySelector('.nav.nav-pills').appendChild(DOM.appendLiwithA(ctgry[i]))
 }
 DOM.initialLoad()
-
-
 document.querySelector('.btn.btn-success').onclick = (e) => {
     e.preventDefault()
     DOM.addProdSpec()
+}
+document.querySelector('.btn.btn-danger').onclick = (e) => {
+    e.preventDefault()
+    let inp = document.querySelector(`.${Array.from(e.target.parentElement.classList).join('.')}`).getElementsByTagName('input')
+    for (let i in inp) {
+        inp[i].value = ''
+    }
+}
+document.querySelector('.btn.btn-warning').onclick = (e) => {
+    e.preventDefault()
+    let data = JSON.parse(document.getElementsByTagName('pre')[0].innerHTML)
+    let key = data.key
+    let category = data.category
+    delete data.key
+    delete data.category
+    if (Object.values(data.specs).length !== 0) {
+        let res = DOM.updateData(category, key, data)
+        res.then(c => {
+            console.log(c)
+        })
+    } else {
+        alert('no changes was made')
+    }
 }
